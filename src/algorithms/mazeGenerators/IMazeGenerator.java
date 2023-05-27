fcfda7ab3a9b43dfefe49ed7943440cbcd1ebd67
nettamei <127881444+nettamei@users.noreply.github.com>
2023-05-27 17:59:44 +0300

    Delete src directory

--- src/algorithms/mazeGenerators/IMazeGenerator.java
+++
@@ -1,9 +0,0 @@
-package algorithms.mazeGenerators;
-//interface for generating a maze
-public interface IMazeGenerator {
-    //function to generate a maze
-    Maze generate (int rows, int columns);
-    //function for measuring the time of the algorithm
-    long measureAlgorithmTimeMillis(int rows, int columns);
-
-}
