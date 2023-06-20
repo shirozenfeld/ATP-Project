efa5d1ce0f09b43fb0841ecabeb28715f8964519
nettamei <127881444+nettamei@users.noreply.github.com>
2023-06-20 19:09:20 +0300

    Delete ATP-Project-PartB directory

--- ATP-Project-PartB/src/algorithms/search/ISearchingAlgorithm.java
+++
@@ -1,7 +0,0 @@
-package algorithms.search;
-// interface for a searching algorithm
-public interface ISearchingAlgorithm {
-    public Solution solve(ISearchable domain);
-    public String getName();
-    public int getNumberOfNodesEvaluated();
-}
--- ATP-Project-PartB/src/test/RunSearchOnMaze3D.java
+++
@@ -1,4 +0,0 @@
-package test;
-
-public class RunSearchOnMaze3D {
-}
